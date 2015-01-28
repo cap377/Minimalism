0fc90d45adfdb48c700cb862eb367e868411a825
ksg56-NAU <ksg56@nau.edu>
2015-01-28 12:22:42 -0700

    Massive change-up
    For Cherie and Marcus

--- src/Root.hx
+++ src/Root.hx
@@ -1,13 +1,20 @@
+import flash.media.Sound;
 import starling.display.Sprite;
 import starling.utils.AssetManager;
 import starling.display.Image;
 import starling.core.Starling;
 import starling.animation.Transitions;
+import starling.events.Event;
+import starling.events.KeyboardEvent;
+
+//[Embed(source="../media/sounds/testedit.wav")]
+//public const MOVE_SOUND:Class;
 
 class Root extends Sprite {
 
     public static var assets:AssetManager;
-    public var preGame:Image;
+    public var first_screen:Image;
+
 
     public function new() {
         super();
@@ -16,7 +23,18 @@ class Root extends Sprite {
     public function start(startup:Startup) {
 
         assets = new AssetManager();
-        assets.enqueue("assets/pre game.png");
+	    assets.enqueue("assets/dino_lose.png");
+        assets.enqueue("assets/first_screen_no_pants_or_pencil.png");
+        assets.enqueue("assets/first_screen_no_pants.png");
+        assets.enqueue("assets/first_screen_no_paper.png");
+        assets.enqueue("assets/first_screen_no_pencil.png");
+        assets.enqueue("assets/first_screen.png");
+        assets.enqueue("assets/lose.png");
+        assets.enqueue("assets/ready_for_school.png");
+        assets.enqueue("assets/sleep_lose.png");
+        assets.enqueue("assets/tv_lose.png");
+        assets.enqueue("assets/win_pants.png");
+        assets.enqueue("assets/you_win.png");
         assets.loadQueue(function onProgress(ratio:Float) {
 
             if (ratio == 1) {
@@ -27,10 +45,41 @@ class Root extends Sprite {
                         alpha: 0,
                         onComplete: function() {
                         startup.removeChild(startup.loadingBitmap);
-                        preGame = new Image(Root.assets.getTexture("pre game"));
-                        preGame.x = 0;
-                        preGame.y = 0;
-                        addChild(preGame);
+                        first_screen = new Image(Root.assets.getTexture("first_screen"));
+                        first_screen.x = 0;
+                        first_screen.y = 0;
+						first_screen = new Image(Root.assets.getTexture("first_screen"));
+                        first_screen.x = 0;
+                        first_screen.y = 0;
+						first_screen = new Image(Root.assets.getTexture("first_screen"));
+                        first_screen.x = 0;
+                        first_screen.y = 0;
+						first_screen = new Image(Root.assets.getTexture("first_screen"));
+                        first_screen.x = 0;
+                        first_screen.y = 0;
+						first_screen = new Image(Root.assets.getTexture("first_screen"));
+                        first_screen.x = 0;
+                        first_screen.y = 0;
+						first_screen = new Image(Root.assets.getTexture("first_screen"));
+                        first_screen.x = 0;
+                        first_screen.y = 0;
+						first_screen = new Image(Root.assets.getTexture("first_screen"));
+                        first_screen.x = 0;
+                        first_screen.y = 0;
+						first_screen = new Image(Root.assets.getTexture("first_screen"));
+                        first_screen.x = 0;
+                        first_screen.y = 0;
+                        addChild(first_screen);
+                        addChild(first_screen_no_pants_or_pencil);
+                        addChild(first_screen_no_pants);
+                        addChild(first_screen_no_pencil);
+                        addChild(lose);
+                        addChild(ready_for_school);
+                        addChild(sleep_lose);
+                        addChild(tv_lose);
+                        addChild(win_pants);
+                        addChild(you_win);
+                        addChild(dino_lose);
                     }
 
                 });
@@ -38,5 +87,7 @@ class Root extends Sprite {
 
         });
     }
+	
+
 
 }

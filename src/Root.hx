8c3be64ae297d9f254552085485a9e02856cca50
ksg56-NAU <ksg56@nau.edu>
2015-01-30 00:47:30 -0700

    Final version
    Not the way it should have been, but it's better than nothing.

--- src/Root.hx
+++ src/Root.hx
@@ -11,6 +11,7 @@ class Root extends Sprite {
     public static var assets:AssetManager;
     public var first_screen:Image;
 	public var got_items = 0;
+	public var got_pants = 0;
 
     public function new() {
         super();
@@ -33,11 +34,11 @@ class Root extends Sprite {
                         alpha: 0,
                         onComplete: function() {
                         startup.removeChild(startup.loadingBitmap);
+						Root.assets.playSound("testedit");
                         first_screen = new Image(Root.assets.getTexture("first_screen"));
                         first_screen.x = 0;
                         first_screen.y = 0;
                         addChild(first_screen);
-						Root.assets.playSound("testedit");
                     }
                 });
             }
@@ -76,18 +77,17 @@ class Root extends Sprite {
 
 			});
             
-			got_items += 1;
+			got_items = 1;
 			
 		}
         else if(e.keyCode == 66){
             if (got_items == 1){
+				if (got_pants == 1){
 			
 			assets = new AssetManager();
 			assets.enqueue("assets/you_win.png");
+			assets.enqueue("assets/properwin.mp3");
 			assets.loadQueue(function onProgress(ratio:Float) {
-			
-			
-			Root.assets.playSound("testedit");
 
             if (ratio == 1) {
 
@@ -98,6 +98,7 @@ class Root extends Sprite {
                         onComplete: function() {
                         removeChild(first_screen);
                         first_screen = new Image(Root.assets.getTexture("you_win"));
+						Root.assets.playSound("properwin");
                         first_screen.x = 0;
                         first_screen.y = 0;
                         addChild(first_screen);
@@ -105,12 +106,39 @@ class Root extends Sprite {
                 });
             }
 			});
+			}
+			else{
+				assets = new AssetManager();
+				assets.enqueue("assets/win_pants.png");
+				assets.enqueue("assets/forgotthepants.mp3");
+				assets.loadQueue(function onProgress(ratio:Float) {
+
+				if (ratio == 1) {
+	
+					Starling.juggler.tween(first_screen, 1.5, {
+						transition: Transitions.LINEAR,
+							delay: 0,
+							alpha: 0,
+							onComplete: function() {
+							removeChild(first_screen);
+							Root.assets.playSound("forgotthepants");
+							first_screen = new Image(Root.assets.getTexture("win_pants"));
+							first_screen.x = 0;
+							first_screen.y = 0;
+							addChild(first_screen);
+						}
+					});
+				}
+			});
+				
+			}
 			
 			}
 			else{
 			
 			assets = new AssetManager();
 			assets.enqueue("assets/lose.png");
+			assets.enqueue("assets/forgotpaper.mp3");
 			assets.loadQueue(function onProgress(ratio:Float) {
 
             if (ratio == 1) {
@@ -121,6 +149,7 @@ class Root extends Sprite {
                         alpha: 0,
                         onComplete: function() {
                         removeChild(first_screen);
+						Root.assets.playSound("forgotpaper");
                         first_screen = new Image(Root.assets.getTexture("lose"));
                         first_screen.x = 0;
                         first_screen.y = 0;
@@ -135,6 +164,37 @@ class Root extends Sprite {
 			});
 			
 			}
+		}
+		if(e.keyCode == 67){
+			assets = new AssetManager();
+			assets.enqueue("assets/ready_for_school.png");
+			assets.enqueue("assets/pickuppants.mp3");
+			assets.loadQueue(function onProgress(ratio:Float) {
+
+            if (ratio == 1) {
+
+                Starling.juggler.tween(first_screen, 1.5, {
+                    transition: Transitions.LINEAR,
+                        delay: 0,
+                        alpha: 0,
+                        onComplete: function() {
+                        removeChild(first_screen);
+						Root.assets.playSound("pickuppants");
+                        first_screen = new Image(Root.assets.getTexture("ready_for_school"));
+                        first_screen.x = 0;
+                        first_screen.y = 0;
+                        addChild(first_screen);
+                    }
+					
+
+                });
+				
+            }
+
+			});
+            
+			got_pants = 1;
+			
 		}
 		else if(e.keyCode == 68){
 			assets = new AssetManager();
@@ -152,6 +212,37 @@ class Root extends Sprite {
                         removeChild(first_screen);
 						Root.assets.playSound("dinosaurgameover");
                         first_screen = new Image(Root.assets.getTexture("dino_lose"));
+                        first_screen.x = 0;
+                        first_screen.y = 0;
+                        addChild(first_screen);
+                    }
+					
+
+                });
+				
+            }
+
+			});
+            
+			got_items += 1;
+			
+		}
+		else if(e.keyCode == 69){
+			assets = new AssetManager();
+			assets.enqueue("assets/sleep_lose.png");
+			assets.enqueue("assets/sleepingsound.mp3");
+			assets.loadQueue(function onProgress(ratio:Float) {
+
+            if (ratio == 1) {
+
+                Starling.juggler.tween(first_screen, 1.5, {
+                    transition: Transitions.LINEAR,
+                        delay: 0,
+                        alpha: 0,
+                        onComplete: function() {
+                        removeChild(first_screen);
+						Root.assets.playSound("sleepingsound");
+                        first_screen = new Image(Root.assets.getTexture("sleep_lose"));
                         first_screen.x = 0;
                         first_screen.y = 0;
                         addChild(first_screen);
